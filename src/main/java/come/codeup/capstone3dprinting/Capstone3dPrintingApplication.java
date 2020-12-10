482fd2b3f94c1a2a2e6b99a64b5d5b9bb3b209ae
John McNay <john.w.mcnay@gmail.com>
2020-12-10 10:20:04 -0600

    initial commit

---
+++ src/main/java/come/codeup/capstone3dprinting/Capstone3dPrintingApplication.java
@@ -0,0 +1,13 @@
+package come.codeup.capstone3dprinting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Capstone3dPrintingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Capstone3dPrintingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/come/codeup/capstone3dprinting/Capstone3dPrintingApplicationTests.java
@@ -0,0 +1,13 @@
+package come.codeup.capstone3dprinting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Capstone3dPrintingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
