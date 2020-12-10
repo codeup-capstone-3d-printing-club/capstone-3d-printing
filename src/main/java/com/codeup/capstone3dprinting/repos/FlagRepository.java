5c07fc5d0f14f3f59e401d30c048a60331097c60
John McNay <john.w.mcnay@gmail.com>
2020-12-10 16:27:07 -0600

    created rest of the repos for models

---
+++ src/main/java/com/codeup/capstone3dprinting/repos/FlagRepository.java
@@ -0,0 +1,9 @@
+package com.codeup.capstone3dprinting.repos;
+
+import com.codeup.capstone3dprinting.models.Flag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FlagRepository extends JpaRepository<Flag, Long> {
+
+
+}
