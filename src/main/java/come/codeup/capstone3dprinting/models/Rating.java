367ddc007c6fe5670768e40b8cc3376905ac9e1a
Bianca Reusch <reusch.bianca@gmail.com>
2020-12-10 14:32:42 -0600

    added ratings model

---
+++ src/main/java/come/codeup/capstone3dprinting/models/Rating.java
@@ -0,0 +1,23 @@
+package come.codeup.capstone3dprinting.models;
+
+import javax.persistence.*;
+@Entity
+@Table(name="ratings")
+public class Rating {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private long id;
+
+    @OneToMany
+    @JoinColumn(nullable = false)
+    private User rater_id;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private File file_id;
+
+    @Column(nullable = false)
+    private int rating;
+
+}
