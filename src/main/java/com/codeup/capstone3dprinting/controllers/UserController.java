20e80d18ffd0df4baeec645356a1c1ecb729e806
Bianca Reusch <reusch.bianca@gmail.com>
2020-12-14 11:07:17 -0600

    added constructors to File class

--- src/main/java/com/codeup/capstone3dprinting/controllers/UserController.java
+++ src/main/java/com/codeup/capstone3dprinting/controllers/UserController.java
@@ -1,8 +1,13 @@
 package com.codeup.capstone3dprinting.controllers;
 
+import com.codeup.capstone3dprinting.models.File;
+import com.codeup.capstone3dprinting.models.User;
+import com.codeup.capstone3dprinting.repos.FileRepository;
 import com.codeup.capstone3dprinting.repos.UserRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -10,9 +15,11 @@ class UserController {
 
     // These two next steps are often called dependency injection, where we create a Repository instance and initialize it in the controller class constructor.
     private final UserRepository userDao;
+    private final FileRepository fileDao;
 
-    public UserController(UserRepository userDao) {
+    public UserController(UserRepository userDao, FileRepository fileDao) {
         this.userDao = userDao;
+        this.fileDao = fileDao;
     }
 
     @GetMapping("/users")
@@ -22,9 +29,12 @@ class UserController {
         return "users index page";
     }
 
-    @GetMapping("/profile")
+    @GetMapping("/profile/{id}")
     @ResponseBody
-    public String showProfile() {
+    public String showProfile(@PathVariable long id, Model model) {
+    User userdb = userDao.getOne(id);
+//        File files = fileDao.
+    model.addAttribute("user", userdb);
 
         return "profile";
     }
--- src/main/java/com/codeup/capstone3dprinting/repos/FileRepository.java
+++ src/main/java/com/codeup/capstone3dprinting/repos/FileRepository.java
@@ -4,6 +4,6 @@ import com.codeup.capstone3dprinting.models.File;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FileRepository extends JpaRepository<File, Long> {
-
+  File findByOwner();
 
 }
