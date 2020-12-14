5e73266d44a5cab48a013a2adff2dc81ad67d5a7
Bianca Reusch <reusch.bianca@gmail.com>
2020-12-14 15:09:19 -0600

    user can now edit posts

--- src/main/java/com/codeup/capstone3dprinting/controllers/FileController.java
+++ src/main/java/com/codeup/capstone3dprinting/controllers/FileController.java
@@ -4,10 +4,7 @@ import com.codeup.capstone3dprinting.models.File;
 import com.codeup.capstone3dprinting.repos.FileRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 class FileController {
@@ -21,21 +18,45 @@ class FileController {
 
     @GetMapping("/files")
     public String showAllFiles(Model model) {
-    model.addAttribute("allEntries", fileDao.findAll());
+        model.addAttribute("allEntries", fileDao.findAll());
         return "index";
     }
+
     @GetMapping("/files/{id}")
     public String showPost(@PathVariable long id, Model model) {
         File filedb = fileDao.getOne(id);
         model.addAttribute("file", filedb);
-        model.addAttribute("user",filedb.getOwner());
+        model.addAttribute("user", filedb.getOwner());
         return "files/showFile";
     }
+
     @PostMapping("/files/{id}")
     public String postIndividual(@PathVariable long id, Model model) {
         File filedb = fileDao.getOne(id);
         model.addAttribute("file", filedb);
-        model.addAttribute("user",filedb.getOwner());
+        model.addAttribute("user", filedb.getOwner());
         return "files/showFile";
+    }
+
+    @GetMapping("/files/{id}/edit")
+    public String showEditForm(@PathVariable long id, Model model) {
+        File filedb = fileDao.getOne(id);
+        model.addAttribute("file", filedb);
+        return "files/editFile";
+    }
+
+    @PostMapping("/files/{id}/edit")
+    public String editFilePost(@PathVariable long id, @ModelAttribute File fileEdit) {
+        File file = fileDao.getOne(id);
+        file.setFile_title(fileEdit.getFile_title());
+        file.setDescription(fileEdit.getDescription());
+        fileDao.save(file);
+        return "redirect:/files/" + file.getId();
+    }
+    @PostMapping("/files/{id}/delete")
+    public String deleteFilePost(@PathVariable long id) {
+        fileDao.deleteById(id);
+//        TODO: make this return back to the list of your own file posts
+        return "redirect:/files";
     }
 }
