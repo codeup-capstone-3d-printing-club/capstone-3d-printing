bb797d58d1b555623b6659904107ca5e29d2770c
liliwang <monsi0510@hotmail.com>
2020-12-16 12:32:41 -0600

    Add instruction for filestack API

--- src/main/resources/static/js/filestack.js
+++ src/main/resources/static/js/filestack.js
@@ -1,22 +1,19 @@
 window.addEventListener('DOMContentLoaded', function () {
     const client = filestack.init(FILESTACK_API_KEY);
     const options = {
-        // displayMode: 'inline',
-        // container: '#inline',
         maxFiles: 20,
         uploadInBackground: false,
         onUploadDone: uploadFile
     };
     const picker = client.picker(options);
     const openBtn = document.getElementById('open');
-    // const closeBtn = document.getElementById('close');
     openBtn.addEventListener('click', () => picker.open());
-    // closeBtn.addEventListener('click', () => picker.close());
 
     function uploadFile(result) {
         const fileData = result.filesUploaded[0];
         console.log(fileData);
         console.log(fileData.url);
+        console.log(fileData.mimeType);
         $('#pickedFile').val(fileData.url);
     }
 
