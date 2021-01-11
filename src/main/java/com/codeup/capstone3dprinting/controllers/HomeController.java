7167634ee11cde12d2d81db96e5e9169b27faf2e
Michael Satterfield <michael.f.satterfield@gmail.com>
2021-01-10 20:43:31 -0600

    adjusted messaging, working on top5 controller,fail

--- src/main/java/com/codeup/capstone3dprinting/controllers/HomeController.java
+++ src/main/java/com/codeup/capstone3dprinting/controllers/HomeController.java
@@ -1,10 +1,28 @@
 package com.codeup.capstone3dprinting.controllers;
 
+import com.codeup.capstone3dprinting.models.File;
+import com.codeup.capstone3dprinting.models.Rating;
+import com.codeup.capstone3dprinting.repos.FileRepository;
+import com.codeup.capstone3dprinting.repos.RatingRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.persistence.GeneratedValue;
+import java.util.List;
 
 @Controller
 public class HomeController {
+    private final RatingRepository ratingDao;
+    private final FileRepository fileDao;
+
+    public HomeController(RatingRepository ratingDao, FileRepository fileDao) {
+        this.ratingDao = ratingDao;
+        this.fileDao = fileDao;
+    }
 
     @GetMapping("/")
     public String landingPage() {
@@ -20,5 +38,12 @@ public class HomeController {
     public String frequentlyAskedPage() {
         return "FAQ";
     }
+//
+//    @GetMapping("/topRated")
+//    public String rating( Model model){
+//        List<File> top5 = fileDao.findTop5ByRatingsOrderByRatings(ratingDao.findAll());
+//        return "home";
+//    }
+
 }
 
