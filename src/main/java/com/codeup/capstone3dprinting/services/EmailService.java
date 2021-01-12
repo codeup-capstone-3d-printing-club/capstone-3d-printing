287c0dcee4a28ddd1c3d2f77acee6640d07f0080
johnwmcnay <john.w.mcnay@gmail.com>
2021-01-12 12:20:52 -0600

    functional web email through mailjet

--- src/main/java/com/codeup/capstone3dprinting/services/EmailService.java
+++ src/main/java/com/codeup/capstone3dprinting/services/EmailService.java
@@ -1,44 +1,58 @@
 package com.codeup.capstone3dprinting.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.mailjet.client.errors.MailjetException;
+import com.mailjet.client.MailjetClient;
+import com.mailjet.client.MailjetRequest;
+import com.mailjet.client.MailjetResponse;
+import com.mailjet.client.ClientOptions;
+import com.mailjet.client.resource.Emailv31;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-@Service("mailService")
+import java.util.Objects;
+
+@Service
 public class EmailService {
 
-    @Autowired
-    public JavaMailSender emailSender;
+    @Value("${mailjet.public.key}")
+    private String publicKey;
 
-    @Value("${spring.mail.from}")
-    private String from;
+    @Value("${mailjet.secret.key}")
+    private String privateKey;
 
-    @Autowired
-    public EmailService(JavaMailSender javaMailSender) {
-        this.emailSender = javaMailSender;
+    public void sendEmail(SimpleMailMessage message) throws MailjetException {
+        MailjetClient client;
+        MailjetRequest request;
+        MailjetResponse response;
+
+        ClientOptions options = ClientOptions.builder()
+                .apiKey(publicKey)
+                .apiSecretKey(privateKey)
+                .build();
+
+        client = new MailjetClient(options);
+
+        String[] emailToArray = message.getTo();
+
+        request = new MailjetRequest(Emailv31.resource)
+                .property(Emailv31.MESSAGES, new JSONArray()
+                        .put(new JSONObject()
+                                .put(Emailv31.Message.FROM, new JSONObject()
+                                        .put("Email", "no-reply@squarecubed.xyz")
+                                        .put("Name", "squarecubed"))
+                                .put(Emailv31.Message.TO, new JSONArray()
+                                        .put(new JSONObject()
+                                                .put("Email", Objects.requireNonNull(emailToArray)[0])))
+                                .put(Emailv31.Message.SUBJECT, message.getSubject())
+                                .put(Emailv31.Message.TEXTPART, message.getText())
+                                .put(Emailv31.Message.CUSTOMID, "SquarecubedNotification")));
+//                              .put(Emailv31.Message.HTMLPART, "<h3>Dear passenger 1, welcome to <a href='https://www.mailjet.com/'>Mailjet</a>!</h3><br />May the delivery force be with you!")
+        response = client.post(request);
+        System.out.println(response.getStatus());
+        System.out.println(response.getData());
+
     }
-
-    @Async
-    public void sendEmail(SimpleMailMessage email) {
-        emailSender.send(email);
-    }
-
-//    public void prepareAndSend(Ad ad, String subject, String body) {
-//        SimpleMailMessage msg = new SimpleMailMessage();
-//        msg.setFrom(from);
-//        msg.setTo(ad.getOwner().getEmail());
-//        msg.setSubject(subject);
-//        msg.setText(body);
-//
-//        try{
-//            this.emailSender.send(msg);
-//        }
-//        catch (MailException ex) {
-//            // simply log it and go on...
-//            System.err.println(ex.getMessage());
-//        }
-//    }
 }
